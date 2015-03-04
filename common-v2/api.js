d4698c99a4b1eb2cd59d4f1fcd302178e06ad54a
vasyas <vasyl@stashuk.com>
2015-03-04 13:30:48 +0200

    bugfixing cookie in forge

--- common-v2/api.js
+++ common-v2/api.js
@@ -43,7 +43,7 @@ internal.priv = {
 	call: function (method, params, success, error) {
 		if ((!internal.debug || window.catalystConnected || method === "internal.showDebugWarning") && (callQueue.length == 0 || handlingQueue)) {
 			var callid = forge.tools.UUID();
-			var onetime = true;
+			var onetime = false; // All method could be called back several times
 			// API Methods which can be calledback multiple times
 			if (method === "button.onClicked.addListener" || method === "message.toFocussed") {
 				onetime = false;
@@ -97,9 +97,9 @@ internal.priv = {
 			}
 
 			var callbacks = temporaryAsyncStorage[result.callid];
-			
+
 			var returnValue = (typeof result.content === "undefined" ? null : result.content);
-			
+
 			if (callbacks && callbacks[result.status]) {
 				callbacks[result.status](result.content);
 			}
@@ -140,7 +140,7 @@ internal.priv = {
 				} catch (e) {}
 			}
 		}
-	}	
+	}
 };
 
 internal.addEventListener = function (event, callback) {
@@ -161,7 +161,7 @@ internal.generateQueryString = function (obj) {
 	if (!(obj instanceof Object)) {
 		return new String(obj).toString();
 	}
-	
+
 	var params = [];
 	var processObj = function (obj, scope) {
 		if (obj === null) {
